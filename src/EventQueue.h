3531580260addadc97c8386b6b04086746cf027b
Marcus Russi <marcusrussi@gmail.com>
2017-07-19 12:48:07 -0400

    Slight refactoring on EventQueue.h in accordance techniques from Stroustrup

--- src/EventQueue.h
+++ src/EventQueue.h
@@ -11,36 +11,52 @@ namespace SIRlib {
 template <enum class Event, typename TimeT>
 class EventQueue {
 public:
-    // Function to schedule stuff
+    // 'schedule' schedules an event 'e' for execution at time 't'. 'params'
+    //   allows passing of a variable number of arguments to the EventGenerator
+    //   associated with event 'e'. The event is generated and added to the
+    //   queue.
     template <typename ...Params>
-    void schedule(TimeT t, Event e, Params&&... params) {
-        EventFunc ef = eventGenerators[e](forward<Params>(params)...);
-
-        return pq->push(ScheduledEvent(t, ef));
-    };
-
-    bool            empty(void) { return pq->empty(); };
-    const_reference top(void)   { return pq->top(); };
-    void            pop(void)   { return pq->pop(); };
+    void schedule(TimeT t, Event e, Params&&... params);
 
     // Alias types
     using EventFunc = function<bool(TimeT, decltype(schedule))>;
 
+    using ScheduledEvent = pair<TimeT, EventFunc>;
+
+    template <typename ...Params>
+    void schedule(TimeT t, Event e, Params&&... params) {
+
+        // Find the event generator and call it with the provded
+        // parameters
+        EventFunc ef = eventGenerators[e](forward<Params>(params)...);
+
+        // Take the resulting event function and schedule it at time 't'
+        return pq->push(ScheduledEvent(t, ef));
+    };
+
+    // The following three methods forward to the underlying data structure
+    //   to allow access to the EventFuncs
+    bool            empty(void) { return pq->empty(); };
+    const_reference top(void)   { return pq->top(); };
+    void            pop(void)   { return pq->pop(); };
+
+    // EventGenerator is a function that takes a variable number of parameters
+    //   and returns an EventFunc
     template <typename ...Params>
     using EventGenerator = function<EventFunc(Params &&...params)>;
 
-    using ScheduledEvent = pair<TimeT, EventFunc>;
-
+    // Allows comparison of ScheduledEvents for insertion into priority queue
     auto ScheduledEventCmp = [](ScheduledEvent left, ScheduledEvent right)
                                { return left.first < right.first; };
 
+    // Specialized priority_queue for storing 'ScheudledEvent's
     using ScheduledEventPQ = \
       priority_queue<ScheduledEvent,
                      vector<ScheduledEvent>,
                      decltype(ScheduledEventCmp)>;
 
     // Constructor
-    EventQueue(map<Event, EventGenerator<TimeT, ...>> _eventGenerators) {
+    EventQueue(map<Event, EventGenerator> _eventGenerators) {
         eventGenerators = _eventGenerators;
         pq = new ScheduledEventPQ(ScheduledEventCmp);
     }
@@ -51,7 +67,7 @@ public:
     }
 
 private:
-    map<Event, EventGenerator<TimeT, ...>> eventGenerators;
+    map<Event, EventGenerator> eventGenerators;
     ScheduledEventPQ *pq;
 };
 
